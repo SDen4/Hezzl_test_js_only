759b46aaae2cec5fbab0282897f1167a2997a796
SDen4 <asw325@yandex.ru>
2020-07-15 19:34:40 +0500

    markup of win & loose pages

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -6,6 +6,7 @@ for (let i = 0; i<units.length; i++) {
     let unit = units[i];
     let maxNum = units.length;
     unit.addEventListener('click', () => {
+        // ход игрока
         if(unit.classList.contains('game__zero') || unit.classList.contains('game__cross')) return;
         field[i] = 1;
         unit.classList.add('game__cross');
@@ -14,11 +15,11 @@ for (let i = 0; i<units.length; i++) {
         // random number from 0 to units.length-1
         let compNum = Math.floor( Math.random()*(maxNum) );
 
+        // ход ИИ
         while (field[compNum] === 1 || field[compNum] === 0) {
             compNum = Math.floor( Math.random()*(maxNum) );
         }
         let unitComp = units[compNum];
-
         field[compNum] = 0;
         unitComp.classList.add('game__zero');
         console.log(field);
