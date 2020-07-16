ce032c61683bc7b14be287123f187fa09caf875e
SDen4 <asw325@yandex.ru>
2020-07-16 19:46:29 +0500

    logic of 8x8 without win

--- src/scripts/gameAction.js
+++ src/scripts/gameAction.js
@@ -2,89 +2,88 @@ import createAddElements from './gameCreateAddElem';
 
 const gameField = document.querySelector('.game__field');
 
-let units = document.querySelectorAll('.game__unit');
 const windowWin = document.querySelector(".game__page_win");
 const windowLose = document.querySelector(".game__page_loose");
 const againBtns = document.querySelectorAll(".game__page_button");
 let field = []; // array of all moves
 let counter = 0; // conter of moves
+let units = document.querySelectorAll('.game__unit');
+
+for (let i = 0; i < units.length; i++) {
+    let unitUser = units[i];
+    unitUser.addEventListener('click', () => {
+        // User's move
+        if (field[i] === 1 || field[i] === 0 || counter >= 16) return;
+        field[i] = 1;
+
+console.log("1 fild = " + i);
 
 
-if(counter < 15) {
-    for (let i = 0; i < units.length; i++) {
-        let unitUser = units[i];
-        unitUser.addEventListener('click', () => {
-            // User's move
-            if (field[i] === 1 || field[i] === 0) return;
-            field[i] = 1;
-            unitUser.classList.add('game__cross');
-            calculateWinner(field);
-    
-            // random number from 0 to units.length-1
-            let maxNum = units.length;
-            let compNum = Math.floor(Math.random() * (maxNum));
-    
-            // AI's move
-            while (field[compNum] === 1 || field[compNum] === 0) {
-                compNum = Math.floor(Math.random() * (maxNum));
+        unitUser.classList.add('game__cross');
+        calculateWinner(field);
+
+        // random number from 0 to units.length-1
+        let maxNum = units.length;
+        let compNum = Math.floor(Math.random() * (maxNum));
+
+        // AI's move
+        while (field[compNum] === 1 || field[compNum] === 0) {
+            compNum = Math.floor(Math.random() * (maxNum));
+        }
+        let unitComp = units[compNum];
+        field[compNum] = 0;
+        unitComp.classList.add('game__zero');
+        calculateWinner(field);
+
+        counter += 2;
+        console.log(counter);
+
+        if (counter == 16) {
+            for (let j = 0; j < field.length; j++) {
+                if (field[j] == undefined) {
+                    field[j] = '';
+                }
             }
-            let unitComp = units[compNum];
-            field[compNum] = 0;
-            unitComp.classList.add('game__zero');
-            calculateWinner(field);
-    
-            counter += 2;
-            console.log(counter);
-            // if(counter > 15) {
+            for (let i = 0; i < 5; i++) {
+                field.splice(5 + 8 * i, 0, '', '', '');
+            };
+            gameField.style.width = "800px";
+            gameField.style.height = "800px";
+            createAddElements();
+        };
 
-            //     // counter = null;
+        units = document.querySelectorAll('.game__unit');
+        for (let j = 0; j < units.length; j++) {
+            let newUnitUser = units[j];
+            if(counter < 16) return;
+            newUnitUser.addEventListener('click', () => {
+                // User's move
+                if (field[j] === 1 || field[j] === 0 || counter < 16) return;
+                field[j] = 1;
+    console.log("2 fild = " + j);
+    console.log("field[j] = " + field[j] + "; " + "j = " + j);
+                newUnitUser.classList.add('game__cross');
+                // calculateWinner(field);
 
-            // }''
-            console.log(field);
-        });
-    };
+                // random number from 0 to units.length-1
+                let newMaxNum = units.length;
+                let newCompNum = Math.floor(Math.random() * (newMaxNum));
+
+                // AI's move
+                while (field[newCompNum] === 1 || field[newCompNum] === 0) {
+                    newCompNum = Math.floor(Math.random() * (newMaxNum));
+                }
+                let newUnitComp = units[newCompNum];
+                field[newCompNum] = 0;
+                newUnitComp.classList.add('game__zero');
+                // calculateWinner(field);
+                console.log(field);
+            });
+
+        };
+        console.log(field);
+    });
 };
-
-if (counter > 15) {
-    createAddElements();
-
-    let newUnits = document.querySelectorAll('.game__unit');
-    console.log(units.length);
-    gameField.style.width = "800px";
-    gameField.style.height = "800px";
-    
-
-    for (let i = 0; i<5; i++) {
-        field.splice(5 + 8*i, 0, null, null, null);
-    };
-
-    for (let i = 0; i < newUnits.length; i++) {
-        let newUnitUser = newUnits[i];
-        newUnitUser.addEventListener('click', () => {
-            console.log('!!!')
-            // User's move
-            if (field[i] === 1 || field[i] === 0) return;
-            field[i] = 1;
-            newUnitUser.classList.add('game__cross');
-            // calculateWinner(field);
-    
-            // random number from 0 to units.length-1
-            let newMaxNum = newUnits.length;
-            let newCompNum = Math.floor(Math.random() * (newMaxNum));
-    
-            // AI's move
-            while (field[newCompNum] === 1 || field[newCompNum] === 0) {
-                newCompNum = Math.floor(Math.random() * (newMaxNum));
-            }
-            let newUnitComp = newUnits[newCompNum];
-            field[newCompNum] = 0;
-            newUnitComp.classList.add('game__zero');
-            // calculateWinner(field);
-
-        });
-    };
-};
-
 
 // calculating winner
 function calculateWinner(field) {
@@ -145,5 +144,3 @@ for (let i = 0; i < againBtns.length; i++) {
         windowWin.classList.remove("game__page_active");
     })
 };
-
-export {counter};
