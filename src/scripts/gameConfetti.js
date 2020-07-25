3a10ccf6bdfbf0c2a8b003ee38c62db905973046
SDen4 <asw325@yandex.ru>
2020-07-25 20:33:53 +0500

    change settings confetti

--- src/scripts/gameConfetti.js
+++ src/scripts/gameConfetti.js
@@ -1,13 +1,13 @@
 const confettiContainer = document.querySelector('.game__page_win');
 
 function createConfetti() {
-    const num = 100; // quantity
+    const num = 50; // quantity
     for (let i = 1; i<=num; i++) {
         let div = document.createElement('div');
         let rnd = Math.random();
         div.className = `game__confetti game__confetti:nth-child(${i})`;
         div.style.background = `rgba(${255*Math.random()}, ${255*Math.random()}, ${255*Math.random()}, ${rnd + .5})`;
-        div.style.left = `${i/num*100}%`;
+        div.style.left = `${i/num*120}%`;
         div.style.animationDelay = `-${i*rnd}s`;
         confettiContainer.append(div);
     };  
