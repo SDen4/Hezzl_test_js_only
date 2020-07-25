2e71c40ab81cfcb571b6bca7f164553562f8a333
SDen4 <asw325@yandex.ru>
2020-07-25 20:25:59 +0500

    refactoring confetti

--- src/scripts/gameConfetti.js
+++ src/scripts/gameConfetti.js
@@ -1,11 +1,14 @@
 const confettiContainer = document.querySelector('.game__page_win');
 
-
 function createConfetti() {
-    const num = 10; // quantity
+    const num = 100; // quantity
     for (let i = 1; i<=num; i++) {
         let div = document.createElement('div');
-        div.className = 'game__confetti';
+        let rnd = Math.random();
+        div.className = `game__confetti game__confetti:nth-child(${i})`;
+        div.style.background = `rgba(${255*Math.random()}, ${255*Math.random()}, ${255*Math.random()}, ${rnd + .5})`;
+        div.style.left = `${i/num*100}%`;
+        div.style.animationDelay = `-${i*rnd}s`;
         confettiContainer.append(div);
     };  
 };
